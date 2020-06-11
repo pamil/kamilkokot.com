0149560a6c9e7b877247e96f81308f26bfbc3cc1
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-11 12:57:25 +0100

    style Signup component
    email input and submit button need improvement on mobile width

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -40,66 +40,100 @@ export default class Signup extends React.Component {
 
   render() {
     const inputStyles = {
-      border: `${rhythm(0.1)} solid #eeeeee`,
-      borderRadius: rhythm(0.5),
+      border: '1px solid rgb(35, 35, 51)',
       padding: `${rhythm(0.1)} ${rhythm(0.4)}`,
-      margin: rhythm(0.01),
+      margin: '0px',
+      flex: 1,
     };
 
     const form = (
-      <form onSubmit={this.handleSubmit} style={{ margin: 0, padding: 0 }}>
+      <form
+        onSubmit={this.handleSubmit}
+        style={{
+          margin: 0,
+          padding: 0,
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+        }}
+      >
         <input
           placeholder="Email address"
           name="email"
           type="email"
           value={this.state.email}
           onChange={this.onEmailChange}
-          style={inputStyles}
+          style={{ ...inputStyles, minHeight: '45px', flex: '0 1 40%' }}
           required
         />
         <input
           type="submit"
           value="Subscribe"
-          style={{ cursor: 'pointer', ...inputStyles }}
+          style={{
+            ...inputStyles,
+            cursor: 'pointer',
+            backgroundImage:
+              'linear-gradient(to right, rgb(35, 35, 51), rgb(62, 62, 108))',
+            border: 'none',
+            color: 'white',
+            textTransform: 'uppercase',
+            letterSpacing: '2px',
+            fontWeight: '300',
+            padding: '10px 17px',
+            flex: '0 1 150px',
+          }}
         />
       </form>
     );
 
     return (
       <div
+        className="gradient-wrapper"
         style={{
-          display: 'block',
-          padding: `${rhythm(0.9)} ${rhythm(0.6)}`,
-          backgroundColor: '#e6e6e6',
-          borderRadius: rhythm(0.5),
-          textAlign: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage:
+            'linear-gradient(to right, rgb(35, 35, 51), rgb(163, 163, 203))',
+          padding: '1px',
+          margin: '50px 0',
         }}
       >
-        <div>
-          <h4 style={{ margin: 0, ...scale(0.3) }}>
-            Subscribe to the newsletter
-          </h4>
-          <span>Stay up to date with my content. Opt-out at any time.</span>
-        </div>
-        <div style={{ padding: `${rhythm(0.8)} 0 0 0` }}>
-          {this.state.failureMessage && (
-            <div
-              style={{
-                backgroundColor: '#eeeeee',
-                borderRadius: rhythm(0.5),
-                padding: `${rhythm(0.5)} ${rhythm(0.2)}`,
-                marginBottom: rhythm(0.8),
-              }}
-            >
-              <div>
-                <strong>We've got a little problem over here!</strong>
+        <div
+          style={{
+            display: 'block',
+            width: '100%',
+            padding: `${rhythm(1.2)} ${rhythm(0.6)}`,
+            backgroundColor: 'white',
+            textAlign: 'center',
+          }}
+        >
+          <div>
+            <h4 style={{ margin: '0 0 15px 0', ...scale(1), fontWeight: 300 }}>
+              Subscribe to the newsletter
+            </h4>
+            <span>Stay up to date with my content. Opt-out at any time.</span>
+          </div>
+          <div style={{ padding: `${rhythm(0.8)} 0 0 0` }}>
+            {this.state.failureMessage && (
+              <div
+                style={{
+                  backgroundColor: '#eeeeee',
+                  borderRadius: rhythm(0.5),
+                  padding: `${rhythm(0.5)} ${rhythm(0.2)}`,
+                  marginBottom: rhythm(0.8),
+                }}
+              >
+                <div>
+                  <strong>We've got a little problem over here!</strong>
+                </div>
+                <div>
+                  <small>{this.state.failureMessage}</small>
+                </div>
               </div>
-              <div>
-                <small>{this.state.failureMessage}</small>
-              </div>
-            </div>
-          )}
-          {this.state.successMessage || form}
+            )}
+            {this.state.successMessage || form}
+          </div>
         </div>
       </div>
     );
